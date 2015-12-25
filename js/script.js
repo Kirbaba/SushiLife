520281d7ac9da2968144bccd380ad7e196ee6801
Vladislav <v.kirbaba@gmail.com>
2015-12-25 16:29:42 +0300

    Reviews

--- js/script.js
+++ js/script.js
@@ -79,4 +79,43 @@ jQuery(function ($) {
             }
         ]
     });
+
+    jQuery('.reviews__carousel').slick({
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        adaptiveHeight: true,
+        speed: 300,
+        arrows: true,        
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        focusOnSelect: false,
+        prevArrow: '<i class="fa fa-chevron-left"></i>',
+        nextArrow: '<i class="fa fa-chevron-right"></i>',
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 770,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
 });
