c04b176a3a9542171acb379aa2d43ea6969ae65e
morozovigor <perffectgame@gmail.com>
2015-12-26 12:12:52 +0300

    category empty

---
+++ category.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="main__wrapper">
+
+        </div>
+        <?php get_sidebar(); ?>
+    </div>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
